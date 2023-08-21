45c20704d2e769a89e355249da685c43f77ec0a0
victor90braz <victor.90braz@gmail.com>
2023-08-21 15:58:41 +0200

    Render a list of products

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
-  const [fact, setFact] = useState("loren ipsum");
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("https://catfact.ninja/fact")
+    fetch("https://itx-frontend-test.onrender.com/api/product")
       .then((response) => response.json())
-      .then((data) => setFact(data.fact));
+      .then((data) => setProducts(data));
   }, []);
 
   return (
     <main>
-      <h1>App de gatitos</h1>
-      <h2>{fact}</h2>
+      <h1>List of products</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <h2>{product.brand}</h2>
+            <p>Model: {product.model}</p>
+            <p>Price: {product.price}</p>
+            <img
+              src={product.imgUrl}
+              alt={`${product.brand} ${product.model}`}
+            />
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
