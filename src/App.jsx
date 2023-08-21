93c0ed857b1237499a1dc5101e23937b31aff6e7
victor90braz <victor.90braz@gmail.com>
2023-08-21 15:48:13 +0200

    Fetch and display random cat fact using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { useEffect, useState } from "react";
+
 const App = () => {
+  const [fact, setFact] = useState("loren ipsum");
+
+  useEffect(() => {
+    fetch("https://catfact.ninja/fact")
+      .then((response) => response.json())
+      .then((data) => setFact(data.fact));
+  }, []);
+
   return (
     <main>
-      <h1>hola</h1>
-      <h2>mondo</h2>
+      <h1>App de gatitos</h1>
+      <h2>{fact}</h2>
     </main>
   );
 };
